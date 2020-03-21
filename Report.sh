bca336c4aedf28a96cc9e52bc02a9615b3ea164f
jafounet <48159294+jafounet@users.noreply.github.com>
2020-03-21 15:35:52 +0100

    Update Report.sh

--- Report.sh
+++ Report.sh
@@ -21,9 +21,9 @@ do
     maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
     firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
     #L3
-    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
-    firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 500000" --xp=maqao_L3_$i
+    firefox maqao_L3_$i/RESULTS/*_one_html/index.html&
     #RAM
-    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
-    firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 600000" --xp=maqao_RAM_$i
+    firefox maqao_RAM_$i/RESULTS/*_one_html/index.html&
 done
