27e003f91dd9a80994f623f55b5c7266dd72ab10
jafounet <48159294+jafounet@users.noreply.github.com>
2020-03-21 15:33:16 +0100

    Mise a jour

--- Report.sh
+++ Report.sh
@@ -14,5 +14,16 @@ make all CC=icc OPTI="-xHost -O3" EXE=prog_icc_O3_xHost
 rm kernel.o
 for i in prog*
 do
-    maqao oneview --create-report=one binary=$i run_command="<binary> 300 100 30" --xp=maqao_$i
+    #L1
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 2000" --xp=maqao_L1_$i
+    firefox maqao_L1_$i/RESULTS/*_one_html/index.html&
+    #L2
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
+    firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
+    #L3
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
+    firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
+    #RAM
+    maqao oneview --create-report=one binary=$i run_command="<binary> 10 50 1000 147456" --xp=maqao_L2_$i
+    firefox maqao_L2_$i/RESULTS/*_one_html/index.html&
 done
