79a3b4fcd9187ef68b37b06b189bd8bc824071e6
Alexis Gabiou <alexis.gabiou@uvsq.isty.fr>
2020-04-03 14:17:04 +0200

    Version vectorisé avec compilation icc
    detail cmd compil : -O3 -xHost -qopt-report=2 -qopt-report-phase=vec

--- driver.c
+++ driver.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#define NB_MESURES 2
-#define NB_REPET 100
+#include <emmintrin.h>
+#define NB_MESURES 31
+#define NB_REPET 31
 extern uint64_t rdtsc ();
 
 void init_array(size_t size, float *tab);
 void baseline ( unsigned n , float a [ n ] , float b [ n ] ,
 float c [ n ] , float d [20]) ;
+void baseline_vect (const unsigned n , float * restrict a , float * restrict b , float * restrict c ,float * restrict d);
 int main()
 {
     size_t n;
     int i;
-    float *a,*b,*c,d[20] ;
-
-    n = 500000 ;
-    a = (float*)malloc(n * sizeof(float));
-    b = (float*)malloc(n * sizeof(float));
-    c = (float*)malloc(n * sizeof(float));
-
+    float *a,*b,*c,*d ;
+	float *ap,*bp,*cp,dp[20] ;
+    n = 600000 ;
+    
+		a = _mm_malloc(n*sizeof(float), 32);
+		b = _mm_malloc(n*sizeof(float), 32);
+		c = _mm_malloc(n*sizeof(float), 32);
+		d = _mm_malloc(20*sizeof(float), 32);
+		
+		/*
+		a = (float*)malloc(n * sizeof(float));
+		b = (float*)malloc(n * sizeof(float));
+		c = (float*)malloc(n * sizeof(float));
+		*/
+		/*
+		ap = (float*)malloc(n * sizeof(float));
+		bp = (float*)malloc(n * sizeof(float));
+		cp = (float*)malloc(n * sizeof(float));
+		*/
     for(i = 0 ; i < NB_MESURES ; i++)
     {
         srand(42);
@@ -26,35 +40,92 @@ int main()
         init_array(n, b);
         init_array(n, c);
         init_array(20, d);
+        /*
+        init_array(n, ap);
+        init_array(n, bp);
+        init_array(n, cp);
+        init_array(20, dp);
+        */
+        /*
+        for(int k=0;k<n;k++)
+        {
+			ap[i]=a[i];
+			bp[i]=b[i];
+			cp[i]=c[i];
+		}
+		for(int k=0;k<20;k++)
+        {
+			dp[i]=d[i];
+		}*/
+		//printf("Check: %f %f \n",a[0],ap[0]);
     //warmup
         if(!i)
         {
             for (int j ; j < 10 ; j++)
             {
-                baseline(n, a,b,c,d);
+				//baseline(n, ap,bp,cp,dp);
+                baseline_vect(n, a,b,c,d);
             }
         }
         else
-            baseline(n, a,b,c,d);
+        {
+            //baseline(n, ap,bp,cp,dp);
+            baseline_vect(n, a,b,c,d);
+        }
 
         //performances mesures
         uint64_t t1 = rdtsc();
         for (int k = 0 ; k < NB_REPET ; k++)
-            baseline(n, a,b,c,d);
+        {
+			//baseline(n, ap,bp,cp,dp);
+            baseline_vect(n, a,b,c,d);
+        }
         uint64_t t2 = rdtsc();
         //print performances
         printf("%.2f cycles/itération \n", (float)(t2-t1 ) / ((float) n *  NB_REPET));
+        /*
+        int valid=1;
+        for(int k=0;k<n;k++)
+        {
+			if(ap[k]!=a[k])
+			{
+				valid=0;
+			}
+		}
+		if(valid)
+		{
+			printf("Resultat valide : %f %f \n",a[0],ap[0]);
+		}
+		else
+		{
+			printf("Resultat non valide : %f %f \n",a[0],ap[0]);
+		}
+		*/
+
     }
-    //free tab
-    free(a);
-    free(b);
-    free(c);
+		 //free tab
+		 
+		_mm_free(a);
+		_mm_free(b);
+		_mm_free(c);
+		_mm_free(d);
+		
+		/*
+		free(a);
+		free(b);
+		free(c);
+		*/
+		/* 
+		//free(ap);
+		//free(bp);
+		//free(cp);
+		*/ 
 }
 void init_array(size_t size, float *tab )
 {
     int i ;
     for (i = 0 ; i < size ; i++)
     {
-        tab[i] = rand() / RAND_MAX;
+        tab[i] = 1.0; //(float)rand() / (float)RAND_MAX;
     }
 }
--- kernel.c
+++ kernel.c
@@ -1,4 +1,55 @@
 #include <math.h>
+
+float a_i_exp( float b , float * restrict d)
+{
+	float a=0.0;
+	for (int k =0; k < 20; k ++)
+	{
+		a += exp( b + d[k]);
+    }
+    return a;
+}
+float a_i_mul( float b ,float * restrict d)
+{
+	float a=0.0;
+	for (int k =0; k < 20; k ++)
+    {
+      	a += b * d[k];
+    }
+    return a;
+}
+
+
+void baseline_vect ( unsigned n , float * restrict a  , float * restrict b ,
+float * restrict c , float * restrict d) //(const unsigned n , float * restrict a , float * restrict b , float * restrict c ,float * restrict d)
+{
+    // all elements in b are assumed positive
+    /*
+	float *al=__builtin_assume_aligned(a,32);
+	float *bl=__builtin_assume_aligned(b,32);
+	float *cl=__builtin_assume_aligned(c,32);
+	float *dl=__builtin_assume_aligned(d,32);
+	
+	*/ 
+	unsigned k , i ;
+    for ( k =0; k < 20; k ++)
+    {
+		#pragma ivdep
+		#pragma  vector aligned
+        for ( i =0; i < n ; i ++)
+        {
+            if (b [ i ] < 1.0)
+            {
+                a [ i ] += exp ( b [ i ] + d [ k ]) / c [ i ];
+            } else
+            {
+                a [ i ] += ( b [ i ] * d [ k ]) / c [ i ];
+            }
+        }
+    }
+}
+
+
 void baseline ( unsigned n , float a [ n ] , float b [ n ] ,
 float c [ n ] , float d [20])
 {
